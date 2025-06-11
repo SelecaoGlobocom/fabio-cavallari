31f55ed642ef3201b8deb1489765f73c60708116
Fabio Cavallari <fabio.cavallari94@gmail.com>
2025-06-10 22:51:34 -0300

    remove debug fab

--- DataTrackApp/app/src/main/java/com/example/datatrackapp/presentation/activity/MainActivity.kt
+++ DataTrackApp/app/src/main/java/com/example/datatrackapp/presentation/activity/MainActivity.kt
@@ -5,26 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
-import com.example.datatrackapp.data.dao.HitDao
-import com.example.datatrackapp.plataform.utils.Logger
 import com.example.datatrackapp.presentation.navigation.DataTrackNavHost
 import com.example.datatrackapp.presentation.theme.DataTrackAppTheme
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.get
 import org.koin.androidx.compose.KoinAndroidContext
 
 class MainActivity : ComponentActivity() {
@@ -33,32 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navHostController = rememberNavController()
-            val coroutineScope = rememberCoroutineScope()
             DataTrackAppTheme {
                 KoinAndroidContext {
                     Scaffold(
                         modifier = Modifier.Companion.fillMaxSize(),
-                        floatingActionButton = {
-                            Column {
-                                FloatingActionButton(
-                                    onClick = {
-                                        val hitDao: HitDao = get()
-                                        coroutineScope.launch {
-                                            val hits = hitDao.getAllHits()
-                                            hits.forEach { Logger.log(it) }
-                                        }
-                                        Logger.log("show hits")
-                                    }
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.AutoMirrored.Filled.List,
-                                        contentDescription = "Show Hits"
-                                    )
-                                }
-                                Spacer(Modifier.Companion.height(16.dp))
-                            }
-
-                        },
                     ) { innerPadding ->
                         Box(Modifier.Companion.padding(innerPadding)) {
                             DataTrackNavHost(navHostController)
